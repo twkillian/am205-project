e99072bd2623eb2d96222334cd507d15d4b2ab20
Jonathan Friedman <jonathan.h.friedman@gmail.com>
2015-11-16 12:58:30 -0500

    Fixed bug switching latitude and longitude

--- geoparse.py
+++ geoparse.py
@@ -7,11 +7,10 @@ def read(shp='geofiles/tabblock2010_25_pophu/tabblock2010_25_pophu'):
 	sf = shapefile.Reader(shp)
 	sr = sf.shapeRecords()
 	for i in xrange(10):
-		print """Box coords: {}\nPopulation: {}
-			   Center: {}\nDist: {}\n""".format(sr[i].shape.bbox,
-											    sr[i].record[-1],
-											    center(sr[i].shape.bbox),
-											    dist(sr[i].shape.bbox))
+		print ('Box coords: {}\nPopulation: {}\n'.format(sr[i].shape.bbox,
+											     	     sr[i].record[-1]) +
+			   'Center: {}\nDist: {}\n'.format(center(sr[i].shape.bbox),
+											   dist(sr[i].shape.bbox)))
 
 
 if __name__ == "__main__":
--- helpers.py
+++ helpers.py
@@ -5,15 +5,15 @@ def dist(coords, r=6371000):
 	on a sphere of radius r. Uses haversine formula from
 	https://en.wikipedia.org/wiki/Great-circle_distance#Computational_formulas 
 	"""
-	x1, y1, x2, y2 = coords
-	dphi = abs(x1 - x2)
-	dlambda = abs(y1 - y2)
-	return 2*asin(sqrt((sin(dphi*0.5))**2 + cos(x1)*cos(x2)*(sin(dlambda*0.5))**2))
+	y1, x1, y2, x2 = coords
+	dphi = abs(x2 - x1)
+	dlambda = abs(y2 - y1)
+	return r*2*asin(sqrt((sin(dphi*0.5))**2 + cos(x1)*cos(x2)*(sin(dlambda*0.5))**2))
 
 def center(coords):
 	"""
 	Calculates the center of a box given the coordinates of the lower left corner 
 	and the upper right corner.
 	"""
-	x1, y1, x2, y2 = coords
-	return [(x1 + x2)*0.5, (y1 + y2)*05]
+	y1, x1, y2, x2 = coords
+	return [(y1 + y2)*0.5, (x1 + x2)*0.5]
